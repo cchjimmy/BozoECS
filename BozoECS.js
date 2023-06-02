fa05fe3efe0eb17e767261a6b1c743af1b724ee1
cchjimmy <himhimjimmy@yahoo.com.hk>
2023-06-02 16:33:57 +0100

    reworked the whole ecs

--- BozoECS.js
+++ BozoECS.js
@@ -1,348 +1,91 @@
-const BozoECS = {
-  World: class World {
-    constructor() {
-      this.EntityManager = new BozoECS.EntityManager(this);
-      this.ComponentManager = new BozoECS.ComponentManager;
-      this.SystemManager = new BozoECS.SystemManager(this);
-    }
-    createEntity() {
-      return this.EntityManager.createEntity();
-    }
-    registerComponents(components = []) {
-      this.ComponentManager.registerComponents(components);
-      return this;
-    }
-    registerSystems(systems = []) {
-      for (let i = 0; i < systems.length; i++) {
-        this.SystemManager.registerSystem(systems[i]);
-      }
-      return this;
-    }
-    init() {
-      for (let i = 0; i < this.SystemManager.enabledSystems.length; i++) {
-        this.SystemManager.enabledSystems[i].init(...arguments);
-      }
-    }
-    run() {
-      for (let i = 0; i < this.SystemManager.enabledSystems.length; i++) {
-        this.SystemManager.enabledSystems[i].run(...arguments);
-      }
-    }
-  },
-  EntityManager: class EntityManager {
-    constructor(world) {
-      this.world = world;
-      this.archetypes = [];
-      this.nextId = -1;
-    }
-    createEntity() {
-      this.nextId++;
-      return this.nextId;
-    }
-    /**
-     * creates a new archetype if not already exists and insert it into archetypes table and returns a reference of it. Returns a reference archetype object if already exists
-     * @param {*} componentTypes 
-     * @returns reference of archetype object
-     */
-    createArchetype(componentTypes = []) {
-      let archetype = { ids: [] };
-      for (let i = 0; i < componentTypes.length; i++) {
-        if (componentTypes[i] >= 0) archetype[componentTypes[i]] = [];
-      }
-      return archetype;
-    }
-    insertArchetype(archetype) {
-      return this.findArchetype(archetype) ?? this.archetypes[this.archetypes.push(archetype) - 1];
-    }
-    addComponents(id, components = []) {
-      let oldComponents = this.removeEntity(id);
-      oldComponents.push(...components);
-      let types = this.world.ComponentManager.getComponentTypes(oldComponents);
-      let a = this.createArchetype(types);
-      a = this.insertArchetype(a);
-      a.ids.push(id);
-      for (let i = 0; i < oldComponents.length; i++) {
-        a[types[i]].push(oldComponents[i].constructor.name == 'Function' ? new oldComponents[i] : oldComponents[i]);
-      }
-      return id;
-    }
-    removeComponents(id, components = []) {
-      let oldComponents = this.removeEntity(id);
-      for (let j = 0; j < oldComponents.length; j++) {
-        for (let i = 0; i < components.length; i++) {
-          if (oldComponents[j].constructor.name != components[i].name) continue;
-          oldComponents.splice(j, 1);
-          break;
-        }
-      }
-      this.addComponents(id, oldComponents);
-    }
-    /**
-     * removes entity from archetype table
-     * @param {*} id 
-     * @returns components of the removed entity
-     */
-    removeEntity(id) {
-      let removedComponents = [];
-      let indices = this.findEntity(id);
-      if (!indices) return removedComponents;
-      let a = this.archetypes[indices[0]];
-      a.ids.splice(indices[1], 1);
-      let compTypes = this.getArchetypeTypes(a);
-      for (let i = 0; i < compTypes.length; i++) {
-        removedComponents.push(...a[compTypes[i]].splice(indices[1], 1));
-      }
-      // removes archetype with no entity
-      if (!a.ids.length) this.archetypes.splice(indices[0], 1);
-      return removedComponents;
-    }
-    /**
-     * copy an entity and its components
-     * @param {*} id 
-     * @returns id of newly instantiated entity
-     */
-    instantiate(id) {
-      let indices = this.findEntity(id);
-      if (!indices) return;
-      let a = this.archetypes[indices[0]];
-      let newId = this.createEntity();
-      let compTypes = this.getArchetypeTypes(a);
-      for (let i = 0; i < comptypes.length; i++) {
-        a[compTypes[i]].push(a[compTypes[i]][indices[1]].copy());
-      }
-      return newId;
-    }
-    /**
-     * returns the archetype index housing the entity, and the entity index in the archetype if entity exists
-     * @param {*} id 
-     * @returns [index of archetype, index of entity]
-     */
-    findEntity(id) {
-      for (let i = 0; i < this.archetypes.length; i++) {
-        for (let j = 0; j < this.archetypes[i].ids.length; j++) {
-          if (this.archetypes[i].ids[j] == id) return [i, j];
-        }
-      }
-    }
-    /**
-     * finds archetype in archetypes table
-     * @param {*} archetype 
-     * @returns reference of archetype
-     */
-    findArchetype(archetype) {
-      // credit: https://stackoverflow.com/questions/14368596/how-can-i-check-that-two-objects-have-the-same-set-of-property-names
-      let keys1 = Object.keys(archetype).sort();
-      for (let i = 0; i < this.archetypes.length; i++) {
-        let keys2 = Object.keys(this.archetypes[i]).sort();
-        if (JSON.stringify(keys1) == JSON.stringify(keys2)) return this.archetypes[i];
-      }
-    }
-    getComponents(id, components = []) {
-      let result = [];
-      let indices = this.findEntity(id);
-      if (!indices) return result;
-      let a = this.archetypes[indices[0]];
-      let types = components.length ? this.world.ComponentManager.getComponentTypes(components) : this.world.EntityManager.getArchetypeTypes(a);
-      for (let i = 0; i < types.length; i++) {
-        if (a[types[i]]) result.push(a[types[i]][indices[1]]);
-      }
-      return result;
-    }
-    archetypesWith(componentTypes = []) {
-      let result = [];
-      for (let i = 0; i < this.archetypes.length; i++) {
-        for (let j = 0; j < componentTypes.length; j++) {
-          if (!this.archetypes[i][componentTypes[j]]) break;
-          if (j == componentTypes.length - 1) result.push(this.archetypes[i]);
-        }
-      }
-      return result;
-    }
-    archetypesWithAny(componentTypes = []) {
-      let result = [];
-      for (let i = 0; i < this.archetypes.length; i++) {
-        for (let j = 0; j < componentTypes.length; j++) {
-          if (!this.archetypes[i][componentTypes[j]]) continue;
-          result.push(this.archetypes[i])
-          break;
-        }
-      }
-      return result;
-    }
-    archetypesWithout(componentTypes = []) {
-      let result = [];
-      for (let i = 0; i < this.archetypes.length; i++) {
-        for (let j = 0; j < componentTypes.length; j++) {
-          if (this.archetypes[i][componentTypes[j]]) break;
-          if (j == componentTypes.length - 1) result.push(this.archetypes[i]);
-        }
-      }
-      return result
-    }
-    getArchetypeTypes(archetype) {
-      let types = Object.keys(archetype);
-      // 'ids' in an archetype is always added last, so this should remove the 'ids' key in an archetype
-      types.splice(-1, 1);
-      return types;
-    }
-  },
-  ComponentManager: class ComponentManager {
-    constructor() {
-      this.componentTypes = [];
-    }
-    registerComponents(components) {
-      let types = this.getComponentTypes(components);
-      for (let i = 0; i < types.length; i++) {
-        if (types[i] >= 0) continue;
-        this.componentTypes.push(components[i]);
-      }
-    }
-    /**
-     * find the type of a component class
-     * @param {*} components component classes in an array
-     * @returns component types in an array
-     */
-    getComponentTypes(components = []) {
-      // index = type of component in this particular world
-      let types = new Array(components.length);
-      for (let j = 0; j < components.length; j++) {
-        let comp = components[j].constructor.name == 'Function' ? components[j] : components[j].constructor;
-        for (let i = 0; i < this.componentTypes.length; i++) {
-          if (this.componentTypes[i] != comp) continue;
-          types[j] = i;
-          break;
-        }
-        (types[j] >= 0) || (types[j] = -1);
-      }
-      return types;
-    }
-  },
-  SystemManager: class SystemManager {
-    constructor(world) {
-      this.world = world;
-      this.systems = [];
-      this.enabledSystems = [];
-    }
-    registerSystem(system) {
-      if (this.findSystemType(system) >= 0) return;
-      this.enableSystem(this.systems[this.systems.push(system) - 1]);
-    }
-    findSystemType(system) {
-      return this.systems.indexOf(system);
-    }
-    enableSystem(system) {
-      let index = this.findSystemType(system);
-      if (index < 0) return;
-      this.enabledSystems.push(new this.systems[index](this.world));
-    }
-    disableSystem(system) {
-      let index = this.findSystemType(system);
-      if (index < 0) return;
-      this.enabledSystems.splice(index, 1);
-    }
-  },
-  Component: class Component {
-    // needs to register this component to have a defined type, i.e. 0, 1, 2, 3...
-    copy() {
-      let copy = new this.constructor;
-      for (let type in this) {
-        if (typeof this[type] == 'object') {
-          Object.assign(copy[type], this[type]);
-          continue;
-        }
-        copy[type] = this[type];
-      }
-      return copy;
-    }
-    clone() {
-      return this;
-    }
-  },
-  System: class System {
-    constructor(world) {
-      this.world = world;
-    }
-    init(args) {
+const BozoECS = {};
 
-    }
-    run(args) {
+BozoECS.createWorld = (systems, init = () => {}) => {
+  return {
+    init,
+    systems,
+    animationRequest: null
+  };
+};
 
-    }
-    /**
-     * get entities ids with all the components listed attached
-     * @param {*} components 
-     * @returns an array of entity ids
-     */
-    queryAll(components = []) {
-      let ids = [];
-      let types = this.world.ComponentManager.getComponentTypes(components);
-      let a = this.world.EntityManager.archetypesWith(types);
-      for (let i = 0; i < a.length; i++) {
-        ids.push(...a[i].ids);
-      }
-      return ids;
-    }
-    /**
-     * get entity ids with any of the components listed attached
-     * @param {*} components 
-     * @returns an array of entity ids
-     */
-    queryAny(components = []) {
-      let ids = [];
-      let types = this.world.ComponentManager.getComponentTypes(components);
-      let a = this.world.EntityManager.archetypesWithAny(types);
-      for (let i = 0; i < a.length; i++) {
-        ids.push(...a[i].ids);
-      }
-      return ids;
-    }
-    /**
-     * get entity ids with the exact list of components attached
-     * @param {*} components 
-     * @returns an array of entity ids
-     */
-    queryOnly(components = []) {
-      let types = this.world.ComponentManager.getComponentTypes(components);
-      let a = this.world.EntityManager.createArchetype(types);
-      a = this.world.EntityManager.findArchetype(a);
-      return a ? [...a.ids] : [];
-    }
-    /**
-     * get entity ids without any of the component listed attached
-     * @param {*} components 
-     * @returns an array of entity ids
-     */
-    queryNot(components = []) {
-      let ids = [];
-      let types = this.world.ComponentManager.getComponentTypes(components);
-      let a = this.world.EntityManager.archetypesWithout(types);
-      for (let i = 0; i < a.length; i++) {
-        ids.push(...a[i].ids);
-      }
-      return ids;
-    }
-    forEach(components = [], func = () => { }) {
-      let types = this.world.ComponentManager.getComponentTypes(components);
-      let table = { ids: [] };
-      for (let i = 0; i < types.length; i++) {
-        table[types[i]] = [];
-      }
-      let a = this.world.EntityManager.archetypesWith(types);
-      for (let i = 0; i < a.length; i++) {
-        table.ids.push(...a[i].ids);
-        for (let j = 0; j < types.length; j++) {
-          table[types[j]].push(...a[i][types[j]]);
-        }
-      }
-      for (let i = 0; i < table.ids.length; i++) {
-        let comps = [];
-        for (let j = 0; j < types.length; j++) {
-          comps.push(table[types[j]][i]);
-        }
-        func(...comps);
-      }
-    }
+BozoECS.createSystem = (update = () => {}) => {
+  return {
+    update
+  };
+}
+
+BozoECS.createComponent = (name, properties) => {
+  return {
+    name,
+    properties
   }
+}
+
+BozoECS.nextId = -1;
+
+BozoECS.createEntities = (amount = 1) => {
+  let entities = new Array(amount);
+  for (let i = 0; i < amount; i++) {
+    BozoECS.nextId++;
+    entities[i] = {
+      id: BozoECS.nextId,
+      components: new Map
+    };
+  }
+  return entities;
+}
+
+BozoECS.simulate = (world, initialize = true) => {
+  if (initialize) {
+    world.archetypes = {};
+    let entities = world.init();
+    
+    world.entities = structuredClone(entities);
+  }
+  
+  let last = performance.now();
+  world.deltaTime = 0;
+  
+  let update = () => {
+    for (let i = 0; i < world.systems.length; i++) {
+      world.systems[i].update(world);
+    }
+    
+    world.deltaTime = performance.now() - last;
+    last += world.deltaTime;
+    
+    world.animationRequest = requestAnimationFrame(update);
+  }
+  
+  update();
+}
+
+BozoECS.pause = (world) => {
+  cancelAnimationFrame(world.animationRequest);
+}
+
+BozoECS.addComponent = (entity, component) => {
+  entity.components.set(component.name, structuredClone(component.properties));
+}
+
+BozoECS.removeComponent = (entity, component) => {
+  entity.components.delete(component.name);
+}
+
+BozoECS.getComponent = (entity, component) => {
+  return entity.components.get(component.name);
+}
+
+BozoECS.forEach = (components, world, callback) => {
+  let result = new Array(components.length);
+  for (let i = 0; i < components.length; i++) {
+    result[i] = world.components[components[i].name];
+  }
+  return result;
+}
+
+BozoECS.sortIntoArchetype = (entity, world) => {
+  
 }
 
 export default BozoECS;
--- example.js
+++ example.js
@@ -1,177 +1,84 @@
 import BozoECS from "./BozoECS.js";
 
-class Transform extends BozoECS.Component {
-  position = {
-    x: 0,
-    y: 0
-  }
-  spawn = {
-    x: 0,
-    y: 0
-  }
-  rotation = 0
-  scale = {
-    x: 1,
-    y: 1
-  }
-}
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+const fps = document.querySelector("span");
 
-class Kinematics extends BozoECS.Component {
-  velocity = {
-    x: 0,
-    y: 0
-  }
-  acceleration = {
-    x: 0,
-    y: -980.7
-  }
-  angularSpeed = 0;
-  time = 0;
-}
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-class Appearance extends BozoECS.Component {
-  color = 'white';
-}
+document.body.appendChild(canvas);
 
-class MovementSystem extends BozoECS.System {
-  init() {
-    this.forEach([Transform, Kinematics], (T, K) => {
-      this.resetEntity(K, T);
-      K.angularSpeed = randomMinMax(-0.1, 0.1);
-    })
-  }
-  resetEntity(k, t) {
-    let maxSpeed = 300;
-    k.velocity.x = (Math.random() - 0.5) * maxSpeed;
-    k.velocity.y = (Math.random() - 0.5) * maxSpeed;
-    k.time = 0;
-    t.spawn.x = (Math.random() - 0.5) * canvas.width;
-    t.spawn.y = (Math.random() - 0.5) * canvas.height;
-    if (mouseDown) {
-      t.spawn.x = mousePos.x;
-      t.spawn.y = mousePos.y;
-    }
-  }
-  run(dt) {
-    this.forEach([Transform, Kinematics], (T, K) => {
-      T.position.x = K.velocity.x*K.time+0.5*K.acceleration.x*K.time**2+T.spawn.x;
-      T.position.y = K.velocity.y*K.time+0.5*K.acceleration.y*K.time**2+T.spawn.y;
-      
-      K.time += dt * parseFloat(speed.value);
-
-      if (T.position.x > canvas.width / 2 || T.position.x < -canvas.width / 2 || T.position.y > canvas.height / 2 || T.position.y < -canvas.height / 2) {
-        this.resetEntity(K, T);
-      };
-
-      T.rotation += K.angularSpeed * parseFloat(speed.value);
-    });
-  }
-}
-
-class RenderSystem extends BozoECS.System {
-  init() {
-    this.forEach([Transform, Appearance], (T, A) => {
-      T.scale.x = randomMinMax(20, 50);
-      T.scale.y = randomMinMax(20, 50);
-      A.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
-    })
-  }
-  run() {
-    ctx.fillStyle = '#133337';
-    ctx.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-
-    this.forEach([Transform, Appearance], (T, A) => {
-      drawRect(T.position.x, T.position.y, T.scale.x, T.scale.y, T.rotation, A.color);
-    })
-
-    function drawRect(x, y, w, h, r, color) {
-      ctx.save();
-      ctx.fillStyle = color;
-      let sin = Math.sin(r);
-      let cos = Math.cos(r);
-      ctx.transform(cos * w, sin * w, -sin * h, cos * h, Math.floor(x), -Math.floor(y));
-      ctx.fillRect(-0.5, -0.5, 1, 1);
-      ctx.restore();
-    }
-  }
-}
-
-function randomMinMax(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-const w = new BozoECS.World;
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
-const fps = document.querySelector('span');
-const numOfEntities = 600;
-var mousePos = {
+let position = BozoECS.createComponent("position", {
   x: 0,
   y: 0
+});
+
+let velocity = BozoECS.createComponent("velocity", {
+  x: 0,
+  y: 0
+});
+
+let appearance = BozoECS.createComponent("appearance", {
+  color: "black",
+  radius: 10
+})
+
+let movement = BozoECS.createSystem((world) => {
+  world.entities.forEach(entity => {
+    let p = BozoECS.getComponent(entity, position);
+    let v = BozoECS.getComponent(entity, velocity);
+    p.x += v.x * world.deltaTime / 1000;
+    p.y += v.y * world.deltaTime / 1000;
+    if (p.x > canvas.width || p.x < 0) p.x = random(0, canvas.width);
+    if (p.y > canvas.height || p.y < 0) p.y = random(0, canvas.height);
+  })
+});
+
+let render = BozoECS.createSystem((world) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  world.entities.forEach(entity => {
+    let p = BozoECS.getComponent(entity, position);
+    let c = BozoECS.getComponent(entity, appearance);
+    ctx.fillStyle = c.color;
+    ctx.beginPath();
+    ctx.moveTo(p.x, p.y);
+    ctx.arc(p.x, p.y, c.radius, 0, Math.PI * 2);
+    ctx.fill();
+  })
+})
+
+let updateFPS = BozoECS.createSystem((world) => {
+  fps.innerText = (1 / (world.deltaTime / 1000)).toFixed(0);
+})
+
+let w = BozoECS.createWorld([render, movement, updateFPS], () => {
+  let entities = BozoECS.createEntities(600);
+  for (let i = 0; i < entities.length; i++) {
+    BozoECS.addComponent(entities[i], position);
+    BozoECS.addComponent(entities[i], velocity);
+    BozoECS.addComponent(entities[i], appearance);
+
+    let v = BozoECS.getComponent(entities[i], velocity);
+    let p = BozoECS.getComponent(entities[i], position);
+    let c = BozoECS.getComponent(entities[i], appearance);
+
+    v.x = random(-100, 100);
+    v.y = random(-100, 100);
+
+    p.x = random(0, canvas.width);
+    p.y = random(0, canvas.height);
+
+    c.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+    c.radius = random(10, 80);
+  }
+  return entities;
+});
+
+BozoECS.simulate(w);
+
+//BozoECS.pause(w);
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
 }
-var mouseDown = false;
-const speed = document.getElementById('speed');
-const speedIndicator = document.querySelectorAll('span')[1];
-
-function init() {
-  document.body.appendChild(canvas);
-
-  resizeCanvas();
-
-  w
-    .registerComponents([Transform, Kinematics, Appearance])
-    .registerSystems([MovementSystem, RenderSystem]);
-
-  for (let i = 0; i < numOfEntities; i++) {
-    w.EntityManager.addComponents(w.createEntity(), [Transform, Kinematics, Appearance]);
-  }
-
-  w.init();
-
-  window.onresize = resizeCanvas;
-
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
-  }
-
-  // mouse support
-  canvas.addEventListener('mousemove', handleMouseMove);
-  canvas.addEventListener('mousedown', handleMousePress);
-  canvas.addEventListener('mouseup', handleMousePress);
-
-  // touch support
-  canvas.addEventListener('touchmove', handleMouseMove);
-  canvas.addEventListener('touchstart', handleMousePress);
-  canvas.addEventListener('touchend', handleMousePress);
-
-  function handleMousePress(e) {
-    mouseDown = !mouseDown;
-    handleMouseMove(e);
-  }
-
-  function handleMouseMove(e) {
-    let clientX = e.clientX ?? e.touches[0].clientX;
-    let clientY = e.clientY ?? e.touches[0].clientY;
-    mousePos.x = clientX - canvas.width / 2;
-    mousePos.y = -(clientY - canvas.height / 2);
-  }
-  
-  speed.oninput = () => {
-    speedIndicator.innerText = parseFloat(speed.value).toFixed(0);
-  }
-}
-
-var past = 0;
-function run() {
-  let now = performance.now();
-  let dt = (now - past) / 1000;
-  w.run(dt);
-  past = now;
-  fps.innerText = (1 / dt).toFixed(0);
-  requestAnimationFrame(run);
-}
-
-init();
-run();
