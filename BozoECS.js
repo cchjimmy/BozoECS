9abb360b8ce85c91b19601a26041891dadd691ea
cchjimmy <himhimjimmy@yahoo.com.hk>
2023-06-10 22:41:15 +0100

    world creation now includes necessary properties for entity archetypes

--- BozoECS.js
+++ BozoECS.js
@@ -1,7 +1,11 @@
 const BozoECS = {};
 
 BozoECS.createWorld = (entities, systems) => {
-  let world = {};
+  let world = {
+    compEnum: {}, // for bitmasking
+    archetypes: {}, // entity components storage
+    archetypeMap: new Map // entity to archetype mapping
+  };
   BozoECS.defineSystems(world, systems);
   BozoECS.addEntities(world, entities);
   return world;
@@ -67,11 +71,7 @@ BozoECS.defineSystems = (world, systems) => {
 }
 
 BozoECS.addEntities = (world, entities) => {
-  world.compEnum ??= {}; // for bitmasking
-  world.archetypes ??= {}; // entity components storage
-  world.archetypeMap ??= new Map;
-  
-  let unique = 0;
+  let unique = Object.keys(world.compEnum).length;
   for (let i = 0; i < entities.length; i++) {
     // figure out archetype
     let archetype = 0;
@@ -91,17 +91,20 @@ BozoECS.addEntities = (world, entities) => {
     
     // otherwise remove entity from old archetype
     if (oldArchetype) {
-      let index = findIndex(world.archetypes[oldArchetype], entities[i].id);
+      let index = BozoECS.findIndex(world.archetypes[oldArchetype], entities[i]);
       for (let prop in world.archetypes[oldArchetype]) {
         world.archetypes[oldArchetype][prop].splice(index, 1);
       }
+      
+      // remove old archetype if it does not contain any entities
+      if (world.archetypes[oldArchetype].ids.length == 0) delete world.archetypes[oldArchetype]; 
     }
     
     // if current archetype does not exist, create a new one
     world.archetypes[archetype] ??= { ids: [] };
 
     // find index of entity
-    let index = findIndex(world.archetypes[archetype], entities[i].id);
+    let index = BozoECS.findIndex(world.archetypes[archetype], entities[i]);
     
     // store entity id
     index = index == -1 ? world.archetypes[archetype].ids.push(entities[i].id) - 1 : index;
@@ -109,20 +112,11 @@ BozoECS.addEntities = (world, entities) => {
     // store components in archetype;
     for (let comp in entities[i].components) {
       world.archetypes[archetype][comp] ??= [];
-      world.archetypes[archetype][comp][index] = entities[i].components[comp];
+      // clone entity components into archetype records
+      world.archetypes[archetype][comp][index] = structuredClone(entities[i].components[comp]);
     }
     
     world.archetypeMap.set(entities[i].id, archetype);
-  }
-  
-  function findIndex(archetype, entityId) {
-    let index = -1;
-    for (let i = 0; i < archetype.ids.length; i++) {
-      if (archetype.ids[i] !== entityId) continue;
-      index = i;
-      break;
-    }
-    return index;
   }
 }
 
@@ -165,8 +159,23 @@ BozoECS.instantiate = (entity) => {
   return e;
 }
 
-BozoECS.copyComponent = (component) => {
-  return BozoECS.createComponent(component.name, component.properties);
+BozoECS.removeEntity = (entity, world) => {
+  let archetype = world.archetypeMap.get(entity.id);
+  if (isNaN(archetype)) return;
+  let index = BozoECS.findIndex(world.archetypes[archetype], entity);
+  for (let prop in world.archetypes[archetype]) {
+    world.archetypes[archetype][prop].splice(index, 1);
+  }
+}
+
+BozoECS.findIndex = (archetype, entity) => {
+  let index = -1;
+  for (let i = 0; i < archetype.ids.length; i++) {
+    if (archetype.ids[i] != entity.id) continue;
+    index = i;
+    break;
+  }
+  return index;
 }
 
 export default BozoECS;
