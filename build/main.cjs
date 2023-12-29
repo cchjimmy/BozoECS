db0bb64740cfe8b5ea192deefcffba55adeebbf3
cchjimmy <92587458+cchjimmy@users.noreply.github.com>
2023-12-29 17:46:03 +0000

    fixed world instantiation

--- build/main.cjs
+++ build/main.cjs
@@ -34,16 +34,16 @@ module.exports = __toCommonJS(exports_exports);
 
 // src/world.js
 function world(...componentsToRegister) {
-  let world2 = {
-    components: {},
-    indexMap: /* @__PURE__ */ new Map(),
-    nextIdx: 0
-  };
-  for (let i = 0; i < componentsToRegister.length; ++i) {
-    world2.components[componentsToRegister[i].id] = [];
-  }
   return (...filtersToRegister) => {
-    world2.filters = new Array(filtersToRegister.length);
+    let world2 = {
+      components: {},
+      indexMap: /* @__PURE__ */ new Map(),
+      nextIdx: 0,
+      filters: new Array(filtersToRegister.length)
+    };
+    for (let i = 0; i < componentsToRegister.length; ++i) {
+      world2.components[componentsToRegister[i].id] = [];
+    }
     for (let i = 0; i < filtersToRegister.length; ++i) {
       world2.filters[i] = {
         mask: filtersToRegister[i],
--- src/world.js
+++ src/world.js
@@ -1,14 +1,14 @@
 export function world(...componentsToRegister) {
-  let world = {
-    components: {},
-    indexMap: new Map(),
-    nextIdx: 0,
-  };
-  for (let i = 0; i < componentsToRegister.length; ++i) {
-    world.components[componentsToRegister[i].id] = [];
-  }
   return (...filtersToRegister) => {
-    world.filters = new Array(filtersToRegister.length);
+    let world = {
+      components: {},
+      indexMap: new Map(),
+      nextIdx: 0,
+      filters: new Array(filtersToRegister.length),
+    };
+    for (let i = 0; i < componentsToRegister.length; ++i) {
+      world.components[componentsToRegister[i].id] = [];
+    }
     for (let i = 0; i < filtersToRegister.length; ++i) {
       world.filters[i] = {
         mask: filtersToRegister[i],
